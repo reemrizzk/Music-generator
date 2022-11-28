14797488dfb755c6c2aa48a8356548ba3c03dbf3
Reem Rizk <50383558+reemrizzk@users.noreply.github.com>
2022-11-28 19:17:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
         <meta name="description" content="AI music melody generator: generate short music melodies!">
         <meta name="keywords" content="Music,generate,generator,ai,artificial,intelligence,melody,song">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="images/favicon.ico" type="image/x-icon">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" crossorigin="anonymous">
         <link rel="stylesheet" href="css/main.css">
         <!--[if IE]>
@@ -15,12 +17,11 @@
 		    </style>  
         <![endif]-->
         <script src="js/jquery.min.js"></script>
-        <script src="js/soundmaker.js"></script>
         <title>AI Music Melody Generator</title>
     </head>
     <body style="margin:16px;">
-        <h1>AI Music Melody Generator 1.2</h1>
-        <p>A simple AI music melody generator in PHP and Javascript</p>
+        <h1>AI Music Melody Generator 1.3</h1>
+        <p>A simple AI music melody generator in PHP and Javascript. <a href="https://github.com/reemrizzk/AI-music-melody-generator">View project on Github</a></p>
         <p>Use this AI music melody generator to generate a simple music melody, in both sound and notation forms. Customize the tempo, key signature, time signature, and more!</p>
         <?php require_once "generator.php"; ?>
         <p>Created with <a href="https://htmlwaveapp.com">HTML Wave</a><br><a href="https://htmlwaveapp.com#contact-us-form">Give feedback</a></p>

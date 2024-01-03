550e2b20415486a1540cc71e674a1171d9022ef2
Reem Rizk <50383558+reemrizzk@users.noreply.github.com>
2024-01-03 13:56:36 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <meta name="description" content="AI music melody generator: generate short music melodies!">
-        <meta name="keywords" content="Music,generate,generator,ai,artificial,intelligence,melody,song">
+        <meta name="description" content="Musicgenerator: generate short music melodies!">
+        <meta name="keywords" content="Music,song,generate,generator">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
         <link rel="icon" href="images/favicon.ico" type="image/x-icon">
@@ -17,13 +17,12 @@
 		    </style>  
         <![endif]-->
         <script src="js/jquery.min.js"></script>
-        <title>AI Music Melody Generator</title>
+        <title>Music Generator</title>
     </head>
     <body style="margin:16px;">
-        <h1>AI Music Melody Generator 1.3</h1>
-        <p>A simple AI music melody generator in PHP and Javascript. <a href="https://github.com/reemrizzk/AI-music-melody-generator">View project on Github</a></p>
-        <p>Use this AI music melody generator to generate a simple music melody, in both sound and notation forms. Customize the tempo, key signature, time signature, and more!</p>
+        <h1>Music Generator 1.3</h1>
+        <p>A music generator written in PHP and Javascript. <a href="https://github.com/reemrizzk/Music-generator">View project on Github</a></p>
+        <p>Use this music generator to generate a simple music melody, in both sound and notation forms. Customize the tempo, key signature, time signature, and more!</p>
         <?php require_once "generator.php"; ?>
-        <p>Created with <a href="https://htmlwaveapp.com">HTML Wave</a><br><a href="https://htmlwaveapp.com#contact-us-form">Give feedback</a></p>
     </body>
 </html>
